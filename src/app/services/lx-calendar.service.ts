1cddb001740417c9bee7f9c25b0520edc2bacf8f
Mayur <securemayur@gmail.com>
2020-11-07 21:54:14 +0530

    Add files via upload
    Added new method to service

--- src/app/services/lx-calendar.service.ts
+++ src/app/services/lx-calendar.service.ts
@@ -37,12 +37,28 @@ export class LXCalendarService {
         this.currentDate.next(date || this.getTodaysDate());
     }
 
+    getCurrentDateDayHeaderLabel() {
+        return this.currentDate.getValue().toFormat('DDD')
+    }
+
+    getCurrentDateMonthHeaderLabel() {
+        return this.currentDate.getValue().toFormat('LLLL y')
+    }
+
     getCurrentDate():BehaviorSubject<DateTime> {
         return this.currentDate;
     }
 
     addMonth(months:number=1) {
         this.setCurrentDate(this.currentDate.getValue().plus({months: months}));
+    }
+
+    addDays(days:number=1) {
+        this.setCurrentDate(this.currentDate.getValue().plus({days}));
+    }
+
+    subtractDays(days:number=1) {
+        this.addDays(days * -1);
     }
 
     subtractMonth(months:number=1) {
@@ -86,5 +102,21 @@ export class LXCalendarService {
             '12 AM','1 AM', '2 AM', '3 AM', '4 AM', '5 AM', '6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM',
             '12 PM','1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM', '9 PM', '10 PM', '11 PM'
         ];
+    }
+
+
+    getScheduledDatesForMonth() {
+        let scheduledList = [];
+        for(let i = 0; i < 12; i++) {
+            let padding =  parseInt((Math.random() * 100).toString()) % 20;
+            let date = this.currentDate.getValue().plus({days:padding});
+            scheduledList.push({
+                startTime: date,
+                endTime: date.plus({minutes:padding}),
+                eventId: i,
+                eventName: `Event ${i}`
+            });
+        }
+        return scheduledList;
     }
 }
